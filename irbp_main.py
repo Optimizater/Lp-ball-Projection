95e1e8f60811f49fd4a6ec61d8c7316dfe8c2d8e
Optimizater <93203671+Optimizater@users.noreply.github.com>
2022-11-28 17:33:19 +0800

    Update irbp_main.py

--- irbp_main.py
+++ irbp_main.py
@@ -14,11 +14,11 @@ import run_irbp_lib
 if __name__ == '__main__':
     
     np.random.seed(2022)   
-    data_dim = np.intc(1e3) # user-specified
+    data_dim = 1e3 # user-specified
     
     p = 0.5 # 0<p<1
     radius = 1. # radius of the lp-ball
-    point_to_be_projected = data_loader.point_projected(data_dim) # follow from N~(0,radius/data_dim)
+    point_to_be_projected = data_loader.point_projected(data_dim) 
     
     x_irbp, dual, runningTime = run_irbp_lib.get_sol(point_to_be_projected,p,radius)
     
